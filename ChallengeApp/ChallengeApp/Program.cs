553a9a7e906b44be3ea6509d2650d8b51b766c57
HalnyGit <haliniakm@gmail.com>
2023-02-05 21:55:51 +0100

    Zadanie dzień 6.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,15 +1,53 @@
-﻿int number = 1568578985;
-string numberAsString = number.ToString();
-char[] letters = numberAsString.ToCharArray();
+﻿using ChallengeApp;
 
-int[] memo = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+Employee employee1 = new Employee("Adam", "Kowalski");
+Employee employee2 = new Employee("Ewa", "Nowak");
+Employee employee3 = new Employee("Michał", "Wiśniewski");
 
-foreach(var elem in letters)
+employee1.AddScore(1);
+employee1.AddScore(4);
+employee1.AddScore(7);
+employee1.AddScore(7);
+employee1.AddScore(9);
+
+employee2.AddScore(6);
+employee2.AddScore(8);
+employee2.AddScore(9);
+employee2.AddScore(6);
+employee2.AddScore(5);
+
+employee3.AddScore(6);
+employee3.AddScore(8);
+employee3.AddScore(9);
+employee3.AddScore(6);
+employee3.AddScore(5);
+
+List<Employee> employees = new List<Employee>();
+employees.Add(employee1);
+employees.Add(employee2); 
+employees.Add(employee3);
+
+List<int> bestResults = new List<int>();
+
+foreach (Employee employee in employees)
 {
-     memo[(int)char.GetNumericValue(elem)]++;
+    bestResults.Add(employee.Result);
 }
 
-for (int i = 0; i < memo.Length; i++)
-{ 
-    Console.WriteLine(i + "=>" + memo[i]);
+int bestResult = bestResults.Max();
+
+List<Employee> topEmployee = new List<Employee>();
+
+foreach (Employee employee in employees)
+{
+    if (employee.Result == bestResult)
+    {
+        topEmployee.Add(employee);
+    }
+}
+
+Console.WriteLine("Pracownik/pracownicy z najwyższymi wynikami:");
+foreach (Employee employee in topEmployee)
+{
+    Console.WriteLine(employee.Name + " " + employee.Surname + " " + employee.Result);
 }
